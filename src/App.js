72401f77fe277f2aabc397b643416e016587c13c
Pratyush Ranjan Srivastava <38191977+Pratyush-Ranjan@users.noreply.github.com>
2019-06-22 23:43:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,16 +23,12 @@ class App extends Component {
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
-    getWeb3
+        getWeb3
     .then(results => {
       this.setState({
         web3: results.web3
       })
-
-      // Instantiate contract once web3 provided.
+          
       this.instantiateContract()
     })
     .catch(() => {
@@ -41,18 +37,10 @@ class App extends Component {
   }
 
   instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
     const contract = require('truffle-contract')
     const simpleStorage = contract(SimpleStorageContract)
     simpleStorage.setProvider(this.state.web3.currentProvider)
 
-    // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       simpleStorage.deployed().then((instance) => {
         this.simpleStorageInstance = instance
@@ -95,7 +83,7 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-          <a href="#" className="pure-menu-heading pure-menu-link">IPFS File Upload DApp</a>
+          <a href="#" className="pure-menu-heading pure-menu-link">IPFS Image Upload DApp</a>
         </nav>
 
         <main className="container">
